fb04a111ff5535199cbcc7362fda83abf10ae9d0
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-05-18 12:02:49 -0300

    GET com parametros

--- Trabalhando-API/main.js
+++ Trabalhando-API/main.js
@@ -7,6 +7,16 @@ function getUsers(){
     .then(data => renderApiResult.textContent = JSON.stringify(data))
     .catch(err => console.error(err))
 }
+function getUser(){
+fetch(`${url}/1`)
+    .then(res => res.json())
+    .then(data => {
+          userName.textContent = data.name
+          userCity.textContent = data.city
+          userAvatar.src = data.avatar 
+    })
+    .catch(err => console.error(err))
+}
 
-
+getUser()
 getUsers()
