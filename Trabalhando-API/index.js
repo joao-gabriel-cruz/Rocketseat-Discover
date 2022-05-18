6b7d236ff8d099531fb0e569998563583c6e1859
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-05-18 11:16:42 -0300

    API do git / Consumindo com axios

--- Trabalhando-API/index.js
+++ Trabalhando-API/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const axios = require('axios')
 
 const app = express()
 
 app.listen('4001')
 
-app.route('/').get( (req,res) => res.send(req.query.nome))
+app.route('/').get( (req,res) =>{
+    axios.get('https://api.github.com/users/joao-gabriel')
+    .then(result => res.send(`<img src="${result.data.avatar_url}"/>`))
+    .catch(err => console.log(err))
+})
 
-app.route('/about/user').get( (req,res) => res.send(req.query))
