b316c56517f06f7bc517267b71eafed6d047cdbd
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-05-18 10:51:18 -0300

    Query Params

--- Trabalhando-API/index.js
+++ Trabalhando-API/index.js
@@ -4,12 +4,6 @@ const app = express()
 
 app.listen('4001')
 
-app.route('/').get( (req,res) => {
-    res.send('hello')
-})
-app.route('/:var').get( (req,res) => {
-    res.send(req.params.var)
-})
-app.route('/identidade/:nome').get( (req,res) => {
-    res.send(req.params.nome)
-})
+app.route('/').get( (req,res) => res.send(req.query.nome))
+
+app.route('/about/user').get( (req,res) => res.send(req.query))
