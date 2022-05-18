7f352b109036eb500a98fe044243b6aa07f6855a
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-05-18 10:07:02 -0300

    DELETE

--- Trabalhando-API/index.js
+++ Trabalhando-API/index.js
@@ -5,14 +5,9 @@ const app = express()
 app.listen('4001')
 
 
-// middleware
-app.use(express.json())
-
 let author = 'João'
 
-app.route('/').get((req,res) => res.send(author))
 
-app.route('/').put((req,res) => {
-    author = req.body.author
-    res.send(author)
+app.route('/:id').delete((req,res) => {
+    res.send(req.params.id)
 })
