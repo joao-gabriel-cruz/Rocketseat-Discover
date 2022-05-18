0afc340c82acebba61e7d3850adb75b10e8d115c
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-05-18 10:34:16 -0300

    Body Params

--- Trabalhando-API/index.js
+++ Trabalhando-API/index.js
@@ -6,12 +6,9 @@ app.listen('4001')
 
 
 // meddleware
-
 app.use(express.json())
 
-
-app.route('/').get((req,res) => res.send(req.query.name))
-
-app.route('/').put((req,res) => res.send(req.body.author))
-
-app.route('/:parametro').get( (req,res) => res.send(req.params.parametro))
+app.route('/').post( (req,res) => {
+    const {nome, cidade} = req.body
+   res.send(`meu nome é ${nome} e minha cidade é ${cidade}`)
+})
